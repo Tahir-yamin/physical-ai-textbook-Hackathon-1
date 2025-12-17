19833f90305169ef4a22f1e0d58a606bfa3c569e
tahir-yamin <tahiryamin2050@gmail.com>
2025-12-17 12:03:10 +0500

    Remove all failed auth protection attempts - clean build

--- textbook/src/theme/NavbarItem/ComponentTypes.tsx
+++
@@ -1,7 +0,0 @@
-import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
-import CustomLanguageSwitcher from './CustomLanguageSwitcher';
-
-export default {
-    ...ComponentTypes,
-    'custom-LanguageSwitcher': CustomLanguageSwitcher,
-};
--- textbook/src/theme/NavbarItem/CustomLanguageSwitcher.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LanguageSwitcher from '@site/src/components/LanguageSwitcher';
-
-export default function NavbarCustomLanguageSwitcher() {
-    return <LanguageSwitcher />;
-}
